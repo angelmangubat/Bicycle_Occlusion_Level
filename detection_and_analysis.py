787724525c07d7cf9a0efc384cc0db3f20d32c96
angelmangubat <116680578+angelmangubat@users.noreply.github.com>
2025-04-07 11:21:14 +0100

    Add files via upload

--- detection_and_analysis.py
+++ detection_and_analysis.py
@@ -1,76 +1,72 @@
 import json
 
+# Constants
 count = 0
 vis_level = 0
-vis_level_w = 0
+wheel_visibility = 0
 vis_level_f = 0
 vis_level_h = 0
 occlusion_level = 0
 wheel_count = 0
 
-path = "Your Local File path"
-filename = path + "detection_result.json"
+# Precompute constants
+occlusion_thresholds = [0.3, 0.5]
+visibility_thresholds = [0.7, 1.0]
+occlusion_messages = ["less than 30%", "less than 50%"]
+visibility_messages = ["more than 50%", "more than 70%"]
 
-# with open(filename) as user_file:
-#    file_contents = user_file.read()
-# print(file_contents)
+
+path = "D:/ATU/dataset/AngeliqueDataset/test_image/"
+filename = path + "test1_result.json"
 
 with open(filename) as user_file:
     inference = json.load(user_file)
 
 # Iterate through the JSON array
-for _predictions in inference["predictions"]:
-    vis_level_w_temp = 0
-    vis_level_w_temp2 = 0
-    im_class = _predictions["class"]
-    im_confidence = _predictions["confidence"]
+for prediction in inference["predictions"]:
+    visibility_temp = 0
+    visibility_temp2 = 0
+    image_class = prediction["class"]
+    image_confidence = prediction["confidence"]
 
-    if im_class == "wheel":
-        im_x = _predictions["width"]
-        im_y = _predictions["height"]
+    if image_class == "wheel":
+        image_width = prediction["width"]
+        image_height = prediction["height"]
+        width_height_ratio = image_width / image_height
 
-        if im_x <= 0.5 * im_y:
-            print(
-                "Prediction [{}] is a [{}] with [{:.2%}] confidence and partially occluded".format(count + 1, im_class,
-                                                                                                   im_confidence))
-            if im_x < 0.3 * im_y:
-                print("The [{}] has occlusion less than 30%".format(im_class))
-                vis_level_w_temp = 0.3
-            else:
-                print("The [{}] has occlusion less than 50%".format(im_class))
-                vis_level_w_temp = 0.5
+        if width_height_ratio <= 0.5:
+            print(f"Prediction [{count + 1}] is a [{image_class}] with [{image_confidence:.2%}] confidence and partially occluded")
+            visibility_temp = occlusion_thresholds[0] if width_height_ratio < 0.3 else occlusion_thresholds[1]
+            occlusion_level = occlusion_messages[0] if visibility_temp == occlusion_thresholds[0] else occlusion_messages[1]
+            print(f"The [{image_class}] has occlusion {occlusion_level}")
         else:
-            print("Prediction [{}] is a [{}] with [{:.2%}] confidence and fully visible".format(count + 1, im_class,
-                                                                                                im_confidence))
-            if im_x > 0.7 * im_y:
-                print("The [{}] has visibility more than 70%".format(im_class))
-                vis_level_w_temp = 1
-            else:
-                print("The [{}] has visibility more than 50%".format(im_class))
-                vis_level_w_temp = 0.7
-        print("Wheel count [{}], size is [{}]x[{}] with visibility [{:.2%}]".format(wheel_count, im_x, im_y,
-                                                                                    0.41 * vis_level_w_temp))
-        vis_level_w_temp2 = 0.41 * vis_level_w_temp
+            print(f"Prediction [{count + 1}] is a [{image_class}] with [{image_confidence:.2%}] confidence and fully visible")
+            visibility_temp = visibility_thresholds[1] if width_height_ratio > 0.7 else visibility_thresholds[0]
+            visibility_level = visibility_messages[1] if visibility_temp == visibility_thresholds[1] else visibility_messages[0]
+            print(f"The [{image_class}] has visibility {visibility_level}")
 
-        wheel_count = wheel_count + 1
-        # Combine wheel visibility level
-        vis_level_w = vis_level_w + vis_level_w_temp2
-        # print("Total Wheel visibility = [{:.2%}]".format(vis_level_w))
+        visibility_temp2 = 0.41 * visibility_temp
+        print(f"Wheel count [{wheel_count}], size is [{image_width}]x[{image_height}] with visibility [{visibility_temp2:.2%}]")
 
-    elif im_class == "frame":
-        print("Prediction [{}] is a [{}] with [{:.2%}] confidence".format(count + 1, im_class, im_confidence))
-        vis_level_f = 1
+        wheel_count += 1
+        wheel_visibility += visibility_temp2
 
-    elif im_class == "handlebar":
-        print("Prediction [{}] is a [{}] with [{:.2%}] confidence".format(count + 1, im_class, im_confidence))
-        vis_level_h = 1
+    elif image_class in ["frame", "handlebar"]:
+        print(f"Prediction [{count + 1}] is a [{image_class}] with [{image_confidence:.2%}] confidence")
+        if image_class == "frame":
+            vis_level_f = 1
+        elif image_class == "handlebar":
+            vis_level_h = 1
 
-    count = count + 1
+    count += 1
 
-print(
-    "Wheel = [{:.2%}], Frame = [{:.0%}], Handlebar = [{:.0%}]".format(vis_level_w, 0.17 * vis_level_f,
-                                                                      0.01 * vis_level_h))
-vis_level = vis_level_w + 0.17 * vis_level_f + 0.01 * vis_level_h
+# Calculate visibility levels
+frame_visibility = 0.17 * vis_level_f
+handlebar_visibility = 0.01 * vis_level_h
+vis_level = wheel_visibility + frame_visibility + handlebar_visibility
 occlusion_level = 1 - vis_level
-print("Bicycle visibility level = [{:.2%}]".format(vis_level))
-print("Bicycle occlusion level = [{:.2%}]".format(occlusion_level))
+
+# Print visibility levels
+print(f"Wheel = [{wheel_visibility:.2%}], Frame = [{frame_visibility:.0%}], Handlebar = [{handlebar_visibility:.0%}]")
+print(f"Bicycle visibility level = [{vis_level:.2%}]")
+print(f"Bicycle occlusion level = [{occlusion_level:.2%}]")
